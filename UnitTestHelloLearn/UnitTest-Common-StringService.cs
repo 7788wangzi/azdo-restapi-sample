19457ce67bdf55b4826a6a5c3e339d8835521af0
WWL Publish <wwlpub@microsoft.com>
2020-03-01 11:15:02 +0800

    added test project

---
+++ UnitTestHelloLearn/UnitTest-Common-StringService.cs
@@ -0,0 +1,50 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using helloLearn.Common;
+using System.Collections.Generic;
+
+namespace UnitTestHelloLearn
+{
+    [TestClass]
+    public class TestCommonStringService
+    {
+        [TestMethod]
+        public void TestReplaceSpaceWithDash()
+        {
+            string inputString = "  There is   no smoke  without fire  ";
+            string expectedString = "There-is-no-smoke-without-fire";
+
+            string actualResult = string.Empty;
+            StringService targetClass = new StringService();
+            targetClass.ReplaceSpaceWithDash(inputString, out actualResult);
+
+            Assert.AreEqual<string>(expectedString, actualResult);
+        }
+
+        [TestMethod]
+        public void TestConvertToLowerCase()
+        {
+            string inputString = "  There is   no smoke  Without fIre  ";
+            string expectedString = "there is   no smoke  without fire";
+
+            string actualResult = string.Empty;
+            StringService targetClass = new StringService();
+            targetClass.ConvertToLowerCase(inputString, out actualResult);
+
+            Assert.AreEqual<string>(expectedString, actualResult);
+        }
+
+        [TestMethod]
+        public void TestRemoveSmallWords()
+        {
+            string inputString = "get a few likes in their WeChat Moments";
+            string expectedString = "get-few-likes-their-wechat-moments";
+            List<string> smallwords = new List<string>() {"in","a","the","and","an" };
+
+            string actualResult = string.Empty;
+            StringService targetClass = new StringService();
+            targetClass.RemoveSmallWords(inputString, out actualResult,smallwords);
+
+            Assert.AreEqual<string>(expectedString, actualResult);
+        }
+    }
+}
---
+++ helloLearn/Common/StringService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helloLearn.Common
+{
+    public class StringService
+    {
+        public void ReplaceSpaceWithDash(string inputStr, out string outputStr)
+        {
+            outputStr = string.Empty;
+            if(!string.IsNullOrEmpty(inputStr))
+            {
+                var words = inputStr.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                StringBuilder newString = new StringBuilder();
+                foreach (string word in words)
+                {
+                    newString.Append(word);
+                    newString.Append("-");
+                }
+
+                if(newString!=null)
+                    outputStr = newString.ToString().Trim('-');
+            }
+
+        }
+
+        public void RemoveSmallWords(string inputStr, out string outputStr, List<string> smallwords)
+        {
+            outputStr = string.Empty;
+            var words = inputStr.Split(new string[] { " ","-" }, StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder newString = new StringBuilder();
+            foreach (string word in words)
+            {
+                if(!smallwords.Contains(word))
+                {
+                    newString.Append(word);
+                    newString.Append("-");
+                }
+            }
+
+            if (newString != null)
+                outputStr = newString.ToString().Trim('-');
+        }
+
+        public void ConvertToLowerCase(string inputStr, out string outputStr)
+        {
+            outputStr = string.Empty;
+            if (!string.IsNullOrEmpty(inputStr))
+            {
+                outputStr = inputStr.Trim().ToLower();
+            }
+        }
+    }
+}
